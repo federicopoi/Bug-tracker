82502a90f1af6b2307d4956ebf3c5b9a3627a70d
Federico Poi <fedepoi@MacBook-Pro-de-Federico.local>
2020-05-19 19:56:15 -0300

    bug-tracker

--- server.js
+++ server.js
@@ -12,10 +12,14 @@ const auth = require("./routes/api/auth");
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Bodyparser Middleware
+app.use(express.json());
+
 // DB Config
 const db = config.get("mongoURI");
 
 // Connect to Mongo
+
 mongoose
   .connect(db, {
     useNewUrlParser: true,
@@ -25,23 +29,22 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-// Bodyparser Middleware
-app.use(express.json());
-
 // User routes
 app.use("/api/items", items);
 app.use("/api/projects", projects);
 app.use("/api/tickets", tickets);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+// Server static assets for production
 
-// Step 3
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
+  // Set static folder
+  app.use(espress.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`)););
