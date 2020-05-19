3ba7c16093bfcea5ecf63da6acc00033f1c73f1e
Federico Poi <fedepoi@MacBook-Pro-de-Federico.local>
2020-05-19 19:24:26 -0300

    bug-tracker-1

--- server.js
+++ server.js
@@ -35,7 +35,10 @@ app.use("/api/users", users);
 app.use("/api/auth", auth);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 5000;
