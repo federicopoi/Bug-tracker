cab3ccc020a2639e4fa6f8f164788194e55ab481
Federico Poi <fedepoi@MacBook-Pro-de-Federico.local>
2020-05-19 17:53:56 -0300

    bug-tracker

--- server.js
+++ server.js
@@ -34,15 +34,6 @@ app.use("/api/projects", projects);
 app.use("/api/tickets", tickets);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
-// Server static assets for production
-
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(espress.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const port = process.env.PORT || 5000;
 app.get("*", (req, res) => {
