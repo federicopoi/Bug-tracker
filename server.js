d9bff6a2a9e510896cb90778d3cc139583e9db93
Federico Poi <fedepoi@MacBook-Pro-de-Federico.local>
2020-05-20 09:31:25 -0300

    bug-tracker

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = config.get("mongoURI");
 
 // Connect to Mongo
 mongoose
-  .connect(db, {
+  .connect(process.env.MONGODB_URI || db, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -36,10 +36,10 @@ app.use("/api/users", users);
 app.use("/api/auth", auth);
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "../client/build")));
+  app.use("/", express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
